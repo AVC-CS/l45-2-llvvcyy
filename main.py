343dbc82bd25afe32859e3d15c5d888442482e23
llqcy <llqcyy05@gmail.com>
2025-04-09 17:29:20 -0700

    done

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    while total <= 100:
+        num = random.randint(1, 10)
+        numbers.append(num)
+        total += num
+    
+    total_excluding_last = total - numbers[-1]
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
+    
     return numbers, total
 
 
